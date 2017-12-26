b2fa83368d5dfbb499b9e54f1edfc05061fd2cd3
Maxim Suntsev <masuntsev@edu.hse.ru>
2017-12-26 20:24:57 +0500

    Сделана форма. Добавлена иконка.

---
+++ Курсовая/MainForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Курсовая
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.InputTab = new System.Windows.Forms.TabPage();
+            this.ResultsTab = new System.Windows.Forms.TabPage();
+            this.Matrix = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.WidthUpDown = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.ResultsButton = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            this.TabControl.SuspendLayout();
+            this.InputTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Matrix)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.WidthUpDown)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.InputTab);
+            this.TabControl.Controls.Add(this.ResultsTab);
+            this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(822, 435);
+            this.TabControl.TabIndex = 0;
+            // 
+            // InputTab
+            // 
+            this.InputTab.Controls.Add(this.tableLayoutPanel1);
+            this.InputTab.Location = new System.Drawing.Point(4, 22);
+            this.InputTab.Name = "InputTab";
+            this.InputTab.Padding = new System.Windows.Forms.Padding(3);
+            this.InputTab.Size = new System.Drawing.Size(814, 409);
+            this.InputTab.TabIndex = 0;
+            this.InputTab.Text = "Ввод данных";
+            this.InputTab.UseVisualStyleBackColor = true;
+            // 
+            // ResultsTab
+            // 
+            this.ResultsTab.Location = new System.Drawing.Point(4, 22);
+            this.ResultsTab.Name = "ResultsTab";
+            this.ResultsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ResultsTab.Size = new System.Drawing.Size(814, 409);
+            this.ResultsTab.TabIndex = 1;
+            this.ResultsTab.Text = "Результаты";
+            this.ResultsTab.UseVisualStyleBackColor = true;
+            // 
+            // Matrix
+            // 
+            this.Matrix.AllowUserToAddRows = false;
+            this.Matrix.AllowUserToDeleteRows = false;
+            this.Matrix.AllowUserToResizeColumns = false;
+            this.Matrix.AllowUserToResizeRows = false;
+            this.Matrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Matrix.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Matrix.Location = new System.Drawing.Point(275, 22);
+            this.Matrix.Name = "Matrix";
+            this.Matrix.RowHeadersWidth = 50;
+            this.Matrix.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.Matrix.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.Matrix.ShowEditingIcon = false;
+            this.Matrix.Size = new System.Drawing.Size(428, 314);
+            this.Matrix.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.53093F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.46907F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Matrix, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(53, 35);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(706, 339);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // WidthUpDown
+            // 
+            this.WidthUpDown.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.WidthUpDown.Location = new System.Drawing.Point(109, 59);
+            this.WidthUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.WidthUpDown.Name = "WidthUpDown";
+            this.WidthUpDown.Size = new System.Drawing.Size(48, 20);
+            this.WidthUpDown.TabIndex = 0;
+            this.WidthUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(43, 16);
+            label1.Margin = new System.Windows.Forms.Padding(3);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(179, 13);
+            label1.TabIndex = 2;
+            label1.Text = "Задайте количество узлов графа:";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.WidthUpDown, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.GenerateButton, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.ResultsButton, 0, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 87);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(266, 184);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 3);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Матрица смежности:";
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.GenerateButton.Location = new System.Drawing.Point(51, 103);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(164, 23);
+            this.GenerateButton.TabIndex = 5;
+            this.GenerateButton.Text = "Сформировать матрицу";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // ResultsButton
+            // 
+            this.ResultsButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ResultsButton.Location = new System.Drawing.Point(50, 141);
+            this.ResultsButton.Name = "ResultsButton";
+            this.ResultsButton.Size = new System.Drawing.Size(165, 23);
+            this.ResultsButton.TabIndex = 6;
+            this.ResultsButton.Text = "Получить результаты";
+            this.ResultsButton.UseVisualStyleBackColor = true;
+            this.ResultsButton.Click += new System.EventHandler(this.ResultsButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 435);
+            this.Controls.Add(this.TabControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Курсовая";
+            this.TabControl.ResumeLayout(false);
+            this.InputTab.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Matrix)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.WidthUpDown)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView Matrix;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.NumericUpDown WidthUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Button ResultsButton;
+        private System.Windows.Forms.TabPage InputTab;
+        private System.Windows.Forms.TabPage ResultsTab;
+        private System.Windows.Forms.TabControl TabControl;
+    }
+}
+
---
+++ Курсовая/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсовая
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Формируем матрицу смежности, заполненную нулями.
+        /// </summary>
+        private void GenerateMatrix()
+        {
+            var width = (int)WidthUpDown.Value;
+
+            // Задаем размеры матрицы смежности
+            Matrix.ColumnCount = width;
+            Matrix.RowCount = 0;
+
+            var row = new List<string>();
+
+            // Заполняем нулями
+            for (int j = 0; j < width; ++j)
+                row.Add("0");
+
+            var array = row.ToArray();
+
+            // Добавляем строки в массив
+            for (int i = 0; i < width; ++i)
+                Matrix.Rows.Add(array);
+
+            // Задаем размеры ячеек
+            for (int i = 0; i < width; ++i)
+            {
+                Matrix.Columns[i].Width = 22;
+                Matrix.Columns[i].HeaderText = (i + 1).ToString();
+                Matrix.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            }
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            GenerateMatrix();
+        }
+
+        private void ResultsButton_Click(object sender, EventArgs e)
+        {
+            TabControl.SelectedTab = TabControl.TabPages[1];
+        }
+    }
+}
